0a5440620e2124783bfa34a84c6a8ea936629e59
David Craig <37514553+dcraig327@users.noreply.github.com>
2024-09-16 20:14:54 -0700

    first commit

---
+++ update-kitty.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# script to install & update kitty binary:
+# https://sw.kovidgoyal.net/kitty/binary/
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
+launch=n
