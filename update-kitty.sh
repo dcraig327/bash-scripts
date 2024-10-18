e152f04c17b6d82b0fd9184fe2149ab091d3fe67
David Craig <37514553+dcraig327@users.noreply.github.com>
2024-10-18 14:09:41 -0700

    add nightly installer

--- update-kitty.sh
+++ update-kitty.sh
@@ -3,5 +3,5 @@
 # script to install & update kitty binary:
 # https://sw.kovidgoyal.net/kitty/binary/
 
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin \
-launch=n
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
+#curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin installer=nightly launch=n
